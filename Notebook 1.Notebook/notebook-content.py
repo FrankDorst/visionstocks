33b7683909d28b8ce81bf026a8c54680b287fe10
Frank Dorst | VisionBI <frank@visionbi.nl>
2024-10-23 14:57:55 +0200

    testing

--- Notebook 1.Notebook/notebook-content.py
+++ Notebook 1.Notebook/notebook-content.py
@@ -1,5 +1,25 @@
+# Fabric notebook source
 
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
 print('hello world')
 
 print("HELLO IS THIS WORKING??")
 
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
